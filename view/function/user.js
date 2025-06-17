78ef040a13417b8fcad00c05cc21ff00b1843290
Cristihan_Lunazco <asnitopro123@gmail.com>
2025-06-17 10:17:35 -0500

    Create user.js
    ...

---
+++ view/function/user.js
@@ -0,0 +1,31 @@
+
+
+function validar_form() {
+    let nro_documento = document.getElementById("nro_identidad").value;
+    let razon_social = document.getElementById("razon_social").value;
+    let telefono= document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+    let departamento = document.getElementById("departamento").value;
+    let provincia = document.getElementById("provincia").value;
+    let distrito = document.getElementById("distrito").value;
+    let cod_postal = document.getElementById("cod_postal").value;
+    let direccion = document.getElementById("direccion").value;
+    let rol = document.getElementById("rol").value;
+
+   if (nro_documento=="" || razon_social=="" || telefono=="" || correo=="" || departamento=="" || provincia=="" || distrito=="" || cod_postal=="" || direccion=="" || rol=="")  {
+    alert("Error: Existen campos vacios");
+    return;
+   }
+   alert("Procederemos a registrar")
+}
+
+// Evita que se envie el formulario xd
+
+if (document.querySelector('#frm_user')) {
+    let frm_user = document.querySelector('#frm_user');
+    frm_user.onsubmit = function(e){
+        e.preventDefault();
+        validar_form();
+    }
+    
+}
