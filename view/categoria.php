ff62551bc67fdc5031860a488fb708aa0093f852
Cristihan_Lunazco <asnitopro123@gmail.com>
2025-06-30 18:54:28 -0500

    -

--- view/categoria.php
+++ view/categoria.php
@@ -59,52 +59,7 @@
     <!-- Bootstrap JS -->
     <script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Script de validación -->
-    <script>
-        function validar_Form() {
-            let nombre = document.getElementById("nombre").value.trim();
-            let detalle = document.getElementById("detalle").value.trim();
 
-            if (nombre === "" || detalle === "") {
-                Swal.fire("Error", "Campos vacíos", "warning");
-                return;
-            }
-            registrarCategoria();
-        }
-
-        if (document.querySelector('#categoriaForm')) {
-            let frm_user = document.querySelector('#categoriaForm');
-            frm_user.onsubmit = function (e) {
-                e.preventDefault();
-                validar_Form();
-            }
-        }
-
-        async function registrarCategoria() {
-            try {
-                const datos = new FormData(document.getElementById('categoriaForm'));
-
-                let respuesta = await fetch(base_url + 'control/categoriaController.php?tipo=registrar', {
-                    method: 'POST',
-                    mode: 'cors',
-                    cache: 'no-cache',
-                    body: datos
-                });
-
-                let json = await respuesta.json();
-
-                if (json.status) {
-                    Swal.fire("Éxito", json.msg, "success");
-                    document.getElementById('categoriaForm').reset();
-                } else {
-                    Swal.fire("Error", json.msg, "error");
-                }
-            } catch (error) {
-                Swal.fire("Error", "Fallo al enviar datos", "error");
-                console.error("Error al registrar categoría: " + error);
-            }
-        }
-    </script>
 </body>
 
 </html>
