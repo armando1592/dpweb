d5a5bb5d68b7af0b3a538d879e0276820ac8453a
Cristihan_Lunazco <asnitopro123@gmail.com>
2025-07-06 13:41:31 -0500

    --

--- control/compra.php
+++ control/compra.php
@@ -6,6 +6,27 @@ $tipo = $_REQUEST['tipo'];
 $objCompra = new compraModel();
 $objPersona = new personaModel();
 if ($tipo=="registrar") {
-
+ //print_r($_POST);
+    //echo $_FILES['imagen']['name'];
     
+     if($_POST);
+   
+    $id_producto= $_POST['id_producto'];
+    $cantidad= $_POST['cantidad'];
+    $precio= $_POST['precio'];
+    $id_trabajador= $_POST['id_trabajador'];
+   
+    if ( $id_producto=="" || $cantidad==""|| $precio==""|| $id_trabajador=="" ) {
+        $arr_Respuesta = array('status'=> false, 'mensaje'=>'error campos vacios');
+    }else {
+        $arrCompra = $objCompra->registrarCompra($id_producto, $cantidad, $precio, $id_trabajador);
+        if ($arrCompra->id>0) {
+            $arr_Respuesta = array('status' => true, 'mensaje' =>'registro exitoso');
+            //cargar archivos
+        } else {
+            $arr_Respuesta = array('status' => false, 'mensaje'=>'error al SUBIR producto');
+        }
+        echo json_encode($arr_Respuesta);
+    } 
+
 }
