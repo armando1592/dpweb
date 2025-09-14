d72de33321255175a7b1b6f1725bc106d2d79920
Cristihan_Lunazco <117657383+armando1592@users.noreply.github.com>
2025-09-14 16:32:58 -0500

    Update productsModel.php
    --

--- model/productsModel.php
+++ model/productsModel.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../library/conexion.php");
-class productsModel{
+class ProductoModel{
     private $conexion;
     function __construct(){
         $this->conexion = new Conexion();
@@ -22,28 +22,23 @@ class productsModel{
         $sql = $this->conexion->query($consulta);
         return $sql->num_rows;
     }
-}require_once("../library/conexion.php");
-class ProductsModel{
-    private $conexion;
-    function __construct(){
-        $this->conexion = new Conexion();
-        $this->conexion = $this->conexion->connect();
-    }
-    public function registrar($codigo, $nombre, $detalle, $precio, $stock, $fecha_vencimiento) {
-        $consulta = "INSERT INTO producto (codigo, nombre, detalle, precio, stock, fecha_vencimiento) VALUE('$codigo', '$nombre', '$detalle', '$precio', '$stock', '$fecha_vencimiento')";
+
+    /*metodo para listar */
+    public function verProductos(){
+        $arr_productos = array();
+        $consulta = "SELECT* from producto";
         $sql = $this->conexion->query($consulta);
-        if ($sql) {
-            $sql = $this->conexion->insert_id;
-        }else{
-            $sql = 0;
+        while ($objeto = $sql->fetch_object()) {
+            array_push($arr_productos, $objeto);
         }
-        return $sql;
+        return $arr_productos;
     }
 
-    public function existeProducto($codigo){
-        $consulta = "SELECT * FROM producto WHERE codigo ='$codigo'";
-        $sql = $this->conexion->query($consulta);
-        return $sql->num_rows;
 
+    /*metodo para ver  */
+    public function ver($id){
+        $consulta = "SELECT * FROM producto WHERE id='$id'";
+        $sql = $this->conexion->query($consulta);
+        return $sql->fetch_object();
     }
 }
