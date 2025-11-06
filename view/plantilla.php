fbb29e7d6a089eb080b4ea9905417472c4c9c361
Cristihan_Lunazco <117657383+armando1592@users.noreply.github.com>
2025-11-05 21:45:23 -0500

    Update plantilla.php

--- view/plantilla.php
+++ view/plantilla.php
@@ -5,7 +5,8 @@ require_once "./control/views_control.php";
 $view = new viewsControl();
 $mostrar = $view->getViewControl();
 
-if ($mostrar == "login" || $mostrar == "404" || $mostrar == "home" ) {
+if ($mostrar == "login" || $mostrar == "404" || $mostrar == "home" ) 
+if ($mostrar == "login" || $mostrar == "404" || $mostrar == "view-clients") {
    require_once "./view/".$mostrar.".php";
 }else{
     include "./view/include/header.php";  //cargamos el header de new-user
